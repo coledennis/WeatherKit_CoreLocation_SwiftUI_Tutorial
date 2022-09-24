e21d0cc3b02ae62af658af7e5e8cb6c631ac8886
Cole Dennis <coleedennis@gmail.com>
2022-09-24 08:37:54 -0700

    Initial Commit

---
+++ WeatherKit_CoreLocation_SwiftUI_Tutorial/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeatherKit_CoreLocation_SwiftUI_Tutorial
+//
+//  Created by Cole Dennis on 9/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherKit_CoreLocation_SwiftUI_Tutorial/WeatherKit_CoreLocation_SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherKit_CoreLocation_SwiftUI_TutorialApp.swift
+//  WeatherKit_CoreLocation_SwiftUI_Tutorial
+//
+//  Created by Cole Dennis on 9/24/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherKit_CoreLocation_SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
